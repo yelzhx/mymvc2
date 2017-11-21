0893b0b0a219e9208032ff4b154656fc590cd616
yelzhas <yelzhx@mail.ru>
2017-11-21 23:58:07 +0600

    Create db.php

---
+++ config/db.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yelzhas
+ * Date: 19.11.2017
+ * Time: 19:53
+ */
+return [
+    'dsn' => 'mysql:host=localhost;dbname=db_mymvc;charset=utf8',
+    'username' => 'root',
+    'password' => '',
+];
